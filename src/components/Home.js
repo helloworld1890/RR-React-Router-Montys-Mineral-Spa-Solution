48a68d7cc8f8caefc19c52e17a07d266c0a0916a
Craig Caparaz <cappuccino1890@Craigs-MacBook-Air.local>
2022-04-13 21:43:58 -0500

    code along

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,15 +1,38 @@
+import { useState } from "react";
+import Modal from "react-bootstrap/Modal";
+import Button from "react-bootstrap/Button";
+export default function Home() {
+  const [show, setShow] = useState(false);
 
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
 
-export default function Home () {
-    return(
-        <div className="homepage">
-            <h1>Visit our resort and change your life!</h1>
+  return (
+    <div className="homepage">
+      <h1>Visit our resort and change your life!</h1>
 
-            <h1>Grand opening in StressVille, Texas</h1>
-            
-            <h1>Check out our wide array of packages and offers!</h1>
+      <h1>Grand opening in StressVille, Texas</h1>
 
-        </div>
-    )
+      <h1>Check out our wide array of packages and offers!</h1>
 
+      <Button variant="primary" onClick={handleShow}>
+        Launch demo modal
+      </Button>
+
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Modal heading</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+          <Button variant="primary" onClick={handleClose}>
+            Save Changes
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </div>
+  );
 }
