e7ea6e5596a0867b7b0761a1d7bba570488f6202
Craig Caparaz <cappuccino1890@Craigs-MacBook-Air.local>
2022-04-13 20:42:22 -0500

    code along

--- src/components/Packages.js
+++ src/components/Packages.js
@@ -1,18 +1,19 @@
-
+import ListGroup from 'react-bootstrap/ListGroup'
+import Container from 'react-bootstrap/Container'
 
 export default function Packages(props) {
     const displayPackages = props.packages.map((eachPackage) => <li>{eachPackage}</li>)
 
     return (
-        <div>
-            <div className="packages">
-                <div className="packagesHeader">
-                    <h1>Our Packages</h1>
-                </div>
-                <ul>
-                    {displayPackages}
-                </ul>
-            </div>
-        </div>
+        <Container>
+    <ListGroup>
+        <ListGroup.Item active>
+            Our Packages
+        </ListGroup.Item>
+        <ListGroup.Item>
+           {displayPackages}
+        </ListGroup.Item>
+    </ListGroup>
+</Container>
     )
 }
